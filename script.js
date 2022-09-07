2ff4c58627d264bc4d5aeb910c93ba4ef5916460
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-08 01:57:50 +0300

    This commit adds a new html layout of the basic appearance of the project. It contains a ul tag for the list of cars

---
+++ script.js
@@ -0,0 +1,48 @@
+// document.addEventListener('DOMContentLoaded', function(event) {
+
+//     event.preventDefault()
+//     fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach(marcas => {
+//             const li = `<li id="li"><h4>${marcas.nome}</h4><span class="stars">★<span><span class="stars">★<span><span class="stars">★<span><span class="stars">★
+//             <span><span class="stars">★<span>
+//             </br>
+//             &nbsp;<button class="btn">♥</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="order">order</button>
+//             </li>`
+  
+
+//             document.querySelector('#ul').insertAdjacentHTML('beforeend', li)
+//         });
+//     })
+
+//        document.getElementsByClassName('order').forEach(addEventListener('click', function(e) {
+//         e.preventDefault()
+//         if (document.getElementById('p').innerHTML === 'paragraph') {
+//             document.getElementById('p').innerHTML = 'I will share the link next time'
+//             document.getElementById('p').style.color ='blue'
+
+            
+//         } else if(document.getElementById('p').innerHTML === 'I will share the link next time'){
+//             document.getElementById('p').innerHTML = 'paragraph'
+//             document.getElementById('p').style.color ='red'
+//         }
+           
+//            console.log(event.target.value)
+//        }))
+
+//        document.getElementById('select').addEventListener('change', function (e) {
+//         e.preventDefault()
+//         let pick = document.getElementById('select').value
+//         if (document.getElementsByTagName('h4').innerHTML[0] = pick) {
+
+//         } 
+//         else {
+            
+//         }
+//        })
+
+// })
+
+        
+
